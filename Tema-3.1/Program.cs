42d1be7852616a6b25d82ec25735841f78ac1278
Andrei Vrancianu <vrancianuandrei@gmail.com>
2019-10-24 10:15:57 +0300

    Update Program.cs

--- Tema-3.1/Program.cs
+++ Tema-3.1/Program.cs
@@ -10,7 +10,54 @@ namespace Tema_3._1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Buna git");
+            //Ex8();
+            //Ex9();
+
+
+
+
+            Console.ReadLine();
+        }
+
+        private static void Ex9()
+        {
+            int i = 3;
+            Console.Write(countSetBits(i));
+        }
+        private static int countSetBits(int n)
+        {
+            int count = 0;
+            while (n > 0)
+                {
+                    count += n & 1;
+                    n >>= 1;
+                }
+            return count;
+            
+        }
+        private static void Ex8()
+        {
+            Console.WriteLine("Enter a number to stop Fibonacci: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int a = displayFibonacci(n);
+
+            Console.WriteLine(a);
+        }
+        private static int displayFibonacci(int num)
+        {
+            int n = num - 1;
+            int[] val = new int[n + 1];
+
+            val[0] = 0;
+            val[1] = 1;
+
+            for (int i = 2; i <= n; i++)
+            {
+                val[i] = val[i - 2] + val[i - 1];
+            }
+
+            return val[n];
+
         }
     }
 }
